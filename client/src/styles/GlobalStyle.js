97e40ac81ce777ebb68d0d3b0a82ddc0bab2232e
SuRyeon-Lee <bunnyatgym@gmail.com>
2022-10-25 16:11:25 +0900

    :lipstick: reset css 작성, sprites 이미지 첨부

--- client/src/styles/GlobalStyle.js
+++ client/src/styles/GlobalStyle.js
@@ -1,14 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset'
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: sans-serif;
-    background-color: #8C8C8C;
-    height: 1000px;
+  ${reset}
+  :root {
   }
-
 `;
 
 export default GlobalStyle;
