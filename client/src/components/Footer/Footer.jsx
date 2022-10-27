d9d23ef69c8a3588f8a6e080dc399a5f75007631
SuRyeon-Lee <bunnyatgym@gmail.com>
2022-10-27 21:53:30 +0900

    :lipstick: style: footer, aside 반응형 디자인 수정

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Categories, Categoriy, SocialsAndCopy } from './style';
+import {
+  Container,
+  Categories,
+  Categoriy,
+  SocialsAndCopy,
+  MediumContainer,
+} from './style';
 import { Link, useLocation } from 'react-router-dom';
 import LayoutContainer from '../LayoutContainer/LayoutContainer';
 
@@ -22,62 +28,64 @@ const Footer = () => {
   return (
     <Container className={needFooter ? '' : 'remove'}>
       <LayoutContainer>
-        <Link to="/">
-          <svg aria-hidden="true" width="32" height="37" viewBox="0 0 32 37">
-            <path d="M26 33v-9h4v13H0V24h4v9h22Z" fill="#BCBBBB"></path>
-            <path
-              d="m21.5 0-2.7 2 9.9 13.3 2.7-2L21.5 0ZM26 18.4 13.3 7.8l2.1-2.5 12.7 10.6-2.1 2.5ZM9.1 15.2l15 7 1.4-3-15-7-1.4 3Zm14 10.79.68-2.95-16.1-3.35L7 23l16.1 2.99ZM23 30H7v-3h16v3Z"
-              fill="#F48024"
-            ></path>
-          </svg>
-        </Link>
-        <Categories>
-          <Categoriy>
-            <p>STACK OVERFLOW</p>
-            <ul>
-              <li>
-                <Link to="/">Questions</Link>
-              </li>
-              <li>Help</li>
-            </ul>
-          </Categoriy>
-          <Categoriy>
-            <p>PRODUCTS</p>
-            <ul>
-              <li>Teams</li>
-              <li>Advertising</li>
-              <li>Collectives</li>
-              <li>Talent</li>
-            </ul>
-          </Categoriy>
-          <Categoriy>
-            <p>COMPANY</p>
-            <ul>
-              <li>About</li>
-              <li>Press</li>
-              <li>Work Here</li>
-              <li>Legal</li>
-              <li>Privacy Policy</li>
-              <li>Terms of Service</li>
-              <li>Contact Us</li>
-              <li>Cookis Settings</li>
-              <li>Cookis Policy</li>
-            </ul>
-          </Categoriy>
-          <Categoriy>
-            <p>STACK EXCHANGE NETWORK</p>
-            <ul>
-              <li>Technology</li>
-              <li>Culture &#38; recreation</li>
-              <li>Life &#38; arts</li>
-              <li>Science</li>
-              <li>Professional</li>
-              <li>Business</li>
-              <li>API</li>
-              <li>Data</li>
-            </ul>
-          </Categoriy>
-        </Categories>
+        <MediumContainer>
+          <Link to="/">
+            <svg aria-hidden="true" width="32" height="37" viewBox="0 0 32 37">
+              <path d="M26 33v-9h4v13H0V24h4v9h22Z" fill="#BCBBBB"></path>
+              <path
+                d="m21.5 0-2.7 2 9.9 13.3 2.7-2L21.5 0ZM26 18.4 13.3 7.8l2.1-2.5 12.7 10.6-2.1 2.5ZM9.1 15.2l15 7 1.4-3-15-7-1.4 3Zm14 10.79.68-2.95-16.1-3.35L7 23l16.1 2.99ZM23 30H7v-3h16v3Z"
+                fill="#F48024"
+              ></path>
+            </svg>
+          </Link>
+          <Categories>
+            <Categoriy>
+              <p>STACK OVERFLOW</p>
+              <ul>
+                <li>
+                  <Link to="/">Questions</Link>
+                </li>
+                <li>Help</li>
+              </ul>
+            </Categoriy>
+            <Categoriy>
+              <p>PRODUCTS</p>
+              <ul>
+                <li>Teams</li>
+                <li>Advertising</li>
+                <li>Collectives</li>
+                <li>Talent</li>
+              </ul>
+            </Categoriy>
+            <Categoriy>
+              <p>COMPANY</p>
+              <ul>
+                <li>About</li>
+                <li>Press</li>
+                <li>Work Here</li>
+                <li>Legal</li>
+                <li>Privacy Policy</li>
+                <li>Terms of Service</li>
+                <li>Contact Us</li>
+                <li>Cookis Settings</li>
+                <li>Cookis Policy</li>
+              </ul>
+            </Categoriy>
+            <Categoriy>
+              <p>STACK EXCHANGE NETWORK</p>
+              <ul>
+                <li>Technology</li>
+                <li>Culture &#38; recreation</li>
+                <li>Life &#38; arts</li>
+                <li>Science</li>
+                <li>Professional</li>
+                <li>Business</li>
+                <li>API</li>
+                <li>Data</li>
+              </ul>
+            </Categoriy>
+          </Categories>
+        </MediumContainer>
         <SocialsAndCopy>
           <li>
             <ul>
