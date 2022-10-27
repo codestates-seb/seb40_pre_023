facbc1ed508d384ac5463e6a20cbb1ccabd05656
lielocks <107406265+lielocks@users.noreply.github.com>
2022-10-27 21:33:55 +0900

    [Feat] Member Mapping (#40)

--- server/src/main/java/com/seb40pre023/domain/member/entity/Member.java
+++ server/src/main/java/com/seb40pre023/domain/member/entity/Member.java
@@ -1,10 +1,16 @@
 package com.seb40pre023.domain.member.entity;
 
+import com.seb40pre023.domain.answer.entity.Answer;
+import com.seb40pre023.domain.answervote.entity.AnswerVote;
+import com.seb40pre023.domain.question.entity.Question;
+import com.seb40pre023.domain.questionvote.entity.QuestionVote;
 import com.seb40pre023.global.common.auditing.BaseTime;
 import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,9 +19,26 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "MEMBER")
 public class Member extends BaseTime {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
     private Long memberId;
+
+    // 멤버와 답변 1:N관계 매핑
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Answer> answers = new ArrayList<>();
+
+    // 멤버와 질문 1:N관계 매핑
+    @OneToMany(mappedBy = "member")
+    private List<Question> questions = new ArrayList<>();
+
+    // 멤버와 답변투표 1:N관계 매핑
+    @OneToMany(mappedBy = "member")
+    private List<AnswerVote> answerVotes = new ArrayList<>();
+
+    // 멤버와 질문투표 1:N관계 매핑
+    @OneToMany(mappedBy = "member")
+    private List<QuestionVote> questionVotes = new ArrayList<>();
+
     @Column(nullable = false, updatable = false)
     private String email;
     @Column(nullable = false)
@@ -45,11 +68,11 @@ public class Member extends BaseTime {
         }
     }
 
-    public Member(Long memberId, String email, String nickname, String about, String img) {
-        this.memberId = memberId;
-        this.email = email;
-        this.nickname = nickname;
-        this.about = about;
-        this.img = img;
-    }
+//    public Member(Long memberId, String email, String nickname, String about, String img) {
+//        this.memberId = memberId;
+//        this.email = email;
+//        this.nickname = nickname;
+//        this.about = about;
+//        this.img = img;
+//    }
 }
