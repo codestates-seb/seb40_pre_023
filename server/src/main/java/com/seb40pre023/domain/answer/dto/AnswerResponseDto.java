f6ddb5ff6fa5149761b9412654c757852ecf4198
lielocks <107406265+lielocks@users.noreply.github.com>
2022-10-27 21:33:55 +0900

    [Feat] Member Mapping (#40)

--- server/src/main/java/com/seb40pre023/domain/answer/dto/AnswerResponseDto.java
+++ server/src/main/java/com/seb40pre023/domain/answer/dto/AnswerResponseDto.java
@@ -1,12 +1,10 @@
 package com.seb40pre023.domain.answer.dto;
 
-import com.seb40pre023.domain.member.entity.Member;
-import com.seb40pre023.domain.question.entity.Question;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.stereotype.Service;
+
 
 @AllArgsConstructor
 @Getter
