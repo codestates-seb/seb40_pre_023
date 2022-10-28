406718cd637e7075d9f36c4797f41e17d6390883
Grooomit <tmd5972@gmail.com>
2022-10-28 14:43:23 +0900

    [Chore] QuestionDto 파일구조 정리
    - QuestionDto
    내부 클래스로 Post, Patch, Response 분리
    
    - QuestionResDto
    Answer 타입 -> AnswerResponseDto 형식으로 변경
    Member 타입 -> MemberResponseDto 형식으로 변경

--- server/src/main/java/com/seb40pre023/domain/question/dto/QuestionPatchDto.java
+++ server/src/main/java/com/seb40pre023/domain/question/dto/QuestionPatchDto.java
@@ -2,7 +2,6 @@ package com.seb40pre023.domain.question.dto;
 
 import com.seb40pre023.domain.question.entity.Question;
 import com.seb40pre023.domain.question.entity.QuestionTag;
-import com.seb40pre023.domain.tag.entity.Tag;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,18 +9,11 @@ import java.util.List;
 
 @Getter
 @Setter
-public class PostQuestionDto {
+public class QuestionPatchDto {
+
+    private Long questionId;
     private String title;
     private String content;
     private List<QuestionTag> tagList;
 
-
-    public Question toQuestion() {
-        Question question = new Question();
-        question.setTitle(title);
-        question.setContent(content);
-        question.setTagList(tagList);
-
-        return question;
-    }
 }
