e506e5f9b7d93cd1ec6bb8e3b8677b60ccf4caef
Hong SeungJae <107091097+Grooomit@users.noreply.github.com>
2022-10-28 15:30:03 +0900

    🎨✨ Feat, Add : Question 생성기능 구현 및 Question 구조 개선 (#50)
    - QuestionDto 생성
      - QuestionDto
    내부 클래스로 Post, Patch, Response 분리
    
      - QuestionResDto
    Answer 타입 -> AnswerResponseDto 형식으로 변경
    Member 타입 -> MemberResponseDto 형식으로 변경
    
    - QuestionMapper 분리
      - 기존 QuestionDto 에서 메서드로 구현했던 Mapper 들을 QuestionMapper 로 한 곳에 정리
    
    - Question 생성 기능 구현
      - QuestionController > postQuestion()
    QuestionPostDto 를 전달받은 후 생성된 Question을 QuestionResDto 형식으로 응답한다.
    
      - QuestionService > createQuestion()
    Controller 로부터 전달받은 Question 을 생성후 DB에 저장하고 반환한다.

--- server/src/main/java/com/seb40pre023/domain/question/service/QuestionService.java
+++ server/src/main/java/com/seb40pre023/domain/question/service/QuestionService.java
@@ -1,6 +1,8 @@
 package com.seb40pre023.domain.question.service;
 
 import com.seb40pre023.domain.member.entity.Member;
+import com.seb40pre023.domain.member.repository.MemberRepository;
+import com.seb40pre023.domain.member.service.MemberService;
 import com.seb40pre023.domain.question.entity.Question;
 import com.seb40pre023.domain.question.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,12 +15,13 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class QuestionService {
     private final QuestionRepository questionRepository;
+    private final MemberService memberService;
 
     // 질문 생성
-    public Question createQuestion(Question question) {
+    public Question createQuestion(Long memberId, Question question) {
 
-//        Member member = memberRepository.findById(memberId);
-//        question.setMember(member);
+        Member member = memberService.findVerifiedMember(memberId);
+        question.setMember(member);
 
         return questionRepository.save(question);
     }
@@ -26,6 +29,8 @@ public class QuestionService {
     // 질문 수정
     public Question updateQuestion(Question question) {
 
+        Optional<Question> findQuestion = questionRepository.findById(question.getQuestionId());
+//        findQuestion.ifPresent();
 
         return question;
     }
