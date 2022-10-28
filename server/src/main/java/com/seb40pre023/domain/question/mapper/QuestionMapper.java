5e8901f86993bbfdc51c1ac3f9516ae4e55a57f9
Hong SeungJae <107091097+Grooomit@users.noreply.github.com>
2022-10-28 15:30:03 +0900

    🎨✨ Feat, Add : Question 생성기능 구현 및 Question 구조 개선 (#50)
    - QuestionDto 생성
      - QuestionDto
    내부 클래스로 Post, Patch, Response 분리
    
      - QuestionResDto
    Answer 타입 -> AnswerResponseDto 형식으로 변경
    Member 타입 -> MemberResponseDto 형식으로 변경
    
    - QuestionMapper 분리
      - 기존 QuestionDto 에서 메서드로 구현했던 Mapper 들을 QuestionMapper 로 한 곳에 정리
    
    - Question 생성 기능 구현
      - QuestionController > postQuestion()
    QuestionPostDto 를 전달받은 후 생성된 Question을 QuestionResDto 형식으로 응답한다.
    
      - QuestionService > createQuestion()
    Controller 로부터 전달받은 Question 을 생성후 DB에 저장하고 반환한다.

---
+++ server/src/main/java/com/seb40pre023/domain/question/mapper/QuestionMapper.java
@@ -0,0 +1,58 @@
+package com.seb40pre023.domain.question.mapper;
+
+import com.seb40pre023.domain.answer.mapper.AnswerMapper;
+import com.seb40pre023.domain.member.mapper.MemberMapper;
+import com.seb40pre023.domain.question.dto.QuestionPatchDto;
+import com.seb40pre023.domain.question.dto.QuestionPostDto;
+import com.seb40pre023.domain.question.dto.QuestionResDto;
+import com.seb40pre023.domain.question.entity.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class QuestionMapper {
+    private final MemberMapper memberMapper;
+    private final AnswerMapper answerMapper;
+
+    public Question questionPostDtoToQuestion(QuestionPostDto questionPostDto) {
+        if (questionPostDto == null) return null;
+
+        Question question = new Question();
+        question.setTitle(questionPostDto.getTitle());
+        question.setContent(questionPostDto.getContent());
+        question.setTagList(questionPostDto.getTagList());
+
+        return question;
+    }
+
+    public Question questionPatchDtoToQuestion(QuestionPatchDto questionPatchDto) {
+        if (questionPatchDto == null) return null;
+
+        Question question = new Question();
+        question.setQuestionId(questionPatchDto.getQuestionId());
+        question.setTitle(questionPatchDto.getTitle());
+        question.setContent(questionPatchDto.getContent());
+        question.setTagList(questionPatchDto.getTagList());
+
+        return question;
+    }
+
+    public QuestionResDto questionToQuestionResDto(Question question) {
+
+        QuestionResDto questionResDto = new QuestionResDto(
+                question.getQuestionId(),
+                memberMapper.memberToMemberResponse(question.getMember()),
+                question.getTitle(),
+                question.getContent(),
+                question.getStatus(),
+                question.getAnswerList().stream()
+                        .map(answerMapper::answerToAnswerResponseDto)
+                        .collect(Collectors.toList()),
+                question.getTagList());
+
+        return questionResDto;
+    }
+}
