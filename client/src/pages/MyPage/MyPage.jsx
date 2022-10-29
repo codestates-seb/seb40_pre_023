ed26be1f444f416c2193928f8eaf067f50398744
skyrain1225 <rudwns9286@gmail.com>
2022-10-29 18:19:47 +0900

    ✨feat: mypage 반응형 추가

--- client/src/pages/MyPage/MyPage.jsx
+++ client/src/pages/MyPage/MyPage.jsx
@@ -8,16 +8,23 @@ import { useState, useEffect } from 'react';
 import Loading from '../../components/Loading/Loading';
 import { InfoAPI } from '../../api/infoapi';
 import tokenState from '../../_state/tokenState';
-
+import isLoginState from '../../_state/isLoginState';
 const MyPage = () => {
   const [memberId, setMemberId] = useRecoilState(memberIdState);
   const [isLoading, setIsLoading] = useState(false);
   const [token, setToken] = useRecoilState(tokenState);
   const [user, setUser] = useState({ data: {} });
-
+  const [isLogin, setIsLogin] = useRecoilState(isLoginState);
   useEffect(() => {
     setIsLoading(true);
     InfoAPI(token).then((res) => {
+      if (res.status === 404) {
+        alert('로그인이 필요한 서비스입니다.');
+        setIsLogin(false);
+        setToken(null);
+        setMemberId(null);
+        window.location.href = window.location.href = '/account/login';
+      }
       setUser(res.data);
       setIsLoading(false);
     });
