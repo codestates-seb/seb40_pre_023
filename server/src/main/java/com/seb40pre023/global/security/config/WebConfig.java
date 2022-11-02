e5229f69e61d54ab8f22cf821ec8d3d537dd8ae5
Grooomit <tmd5972@gmail.com>
2022-11-02 11:56:15 +0900

    [Fix] Question error 해결

---
+++ server/src/main/java/com/seb40pre023/global/security/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.seb40pre023.global.security.config;
+
+import com.seb40pre023.global.security.argumentresolver.LoginIdArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginIdArgumentResolver());
+    }
+}
