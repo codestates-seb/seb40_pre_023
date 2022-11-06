4cac3ca7b831fec29973c0972b42152a323b0627
paquantum <enmw34@naver.com>
2022-11-06 20:28:53 +0900

    [Fix]token expiration times

--- server/src/main/java/com/seb40pre023/global/error/advice/GlobalExceptionAdvice.java
+++ server/src/main/java/com/seb40pre023/global/error/advice/GlobalExceptionAdvice.java
@@ -4,15 +4,12 @@ import com.seb40pre023.global.error.ErrorResponse;
 import com.seb40pre023.global.error.exception.BusinessLogicException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
